78ab85800a997affc5fe8024cff9e7ac17338699
Jair Marques <jairsmarques1126@gmail.com>
2025-06-04 10:37:08 -0300

    adicionando o exercício 02 da aula sobre as estruturas sequenciais em Java

---
+++ 1.1-exercicios-estrutura-sequencial/src/Exercicio02.java
@@ -0,0 +1,25 @@
+/*
+ * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números
+ * com uma mensagem explicativa.
+ */
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x, y, soma;
+		x = sc.nextInt();
+		y = sc.nextInt();
+
+		soma = x + y;
+		System.out.println("SOMA = " + soma);
+
+		sc.close();
+
+	}
+
+}
